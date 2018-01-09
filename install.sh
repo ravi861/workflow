54bada7215e63e1119ac19ec14ffe02e1ead6619
Ravi Vantipalli <rvantipalli@barefootnetworks.com>
2018-01-09 15:31:54 -0800

    upate git-prompt.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp Xresources ~/.Xresources
 cp cscope_maps.vim ~/cscope_maps.vim
+cp git-prompt.sh ~/.git-prompt.sh
 mdkir ~/.vim
 mkdir ~/.vim/syntax
 cp c.vim ~/.vim/syntax/c.vim
