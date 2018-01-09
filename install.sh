135662ab5d20dda3b7f73f03aca5fe43fa1abb0b
Ravi Vantipalli <rvantipalli@barefootnetworks.com>
2018-01-09 15:29:33 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp vimrc ~/.vimrc
+cp tmux.conf ~/.tmux.conf
+cp Xresources ~/.Xresources
+cp cscope_maps.vim ~/cscope_maps.vim
+mdkir ~/.vim
+mkdir ~/.vim/syntax
+cp c.vim ~/.vim/syntax/c.vim
+cp python.vim ~/.vim/syntax/python.vim
