5d28dd1ee8a314d174374ced50f6fb73e3f36e03
Ravi Vantipalli <rvantipalli@barefootnetworks.com>
2018-01-10 16:08:43 -0800

    git-completion

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ cp tmux.conf ~/.tmux.conf
 cp Xresources ~/.Xresources
 cp cscope_maps.vim ~/cscope_maps.vim
 cp git-prompt.sh ~/.git-prompt.sh
+cp git-completion.sh ~/.git-completion.bash
 mdkir ~/.vim
 mkdir ~/.vim/syntax
 cp c.vim ~/.vim/syntax/c.vim
